4da06b634fa4e38c906a6ed7ec2814644f78096e
Alessa Castillo <amcastillo@valdosta.edu>
2023-01-31 15:35:48 -0600

    Update RestControllerApp.java

--- LibraryManagementSystem/src/main/java/com/demo/LibraryManagementSystem/controller/RestControllerApp.java
+++ LibraryManagementSystem/src/main/java/com/demo/LibraryManagementSystem/controller/RestControllerApp.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -37,11 +38,16 @@ public class RestControllerApp {
 		return bookService.getBookById(bookId);
 	}
 	
-	@DeleteMapping("/delete/{bookId}")
+	@DeleteMapping("/deleteBook/{bookId}")
 	public ResponseEntity<String> deleteBook(@PathVariable Integer bookId) {
 		bookService.deleteBookById(bookId);
 		ResponseEntity<String> res = new ResponseEntity<>("Deleted book with bookId: " + bookId, HttpStatus.OK);
 		return res;
+	}
+	
+	@PutMapping("/updateBook")
+	public Book updateBookInfo(@RequestBody Book b) {
+		return bookService.updateBookInfo(b);
 	}
 	
 }
